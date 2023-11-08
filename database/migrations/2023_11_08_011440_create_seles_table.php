f04cae1cd7fcb986c81543708a5df5f39b63e159
Rogerio Pereira <hi@rogeriopereira.dev>
2023-11-07 20:24:01 -0500

    Criando Migrations

---
+++ database/migrations/2023_11_08_011440_create_seles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('seller_id')->constrained();
+            $table->timestamp('sold_at');
+            $table->char('status', 1);
+            $table->integer('total_value'); //Value in cents
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seles');
+    }
+};
